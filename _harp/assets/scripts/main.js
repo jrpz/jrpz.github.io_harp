23a508cfa8e0334ff047fdd304dc9558e3cc7dfc
Jaime Perez <jaime@brlrl.com>
2014-07-27 15:01:03 -0500

    bower setup

--- _harp/assets/scripts/main.js
+++ _harp/assets/scripts/main.js
@@ -1,6 +1,22 @@
-(function(){
-  var window_height = window.innerHeight + 'px',
-      aside = document.getElementsByTagName('aside');
+// Set aside to full height of the window
+var setAsideHeight = function() {
+  var window_height = $(window).height() + 'px';
+  $('aside').css('min-height', window_height);
+};
 
-  aside[0].setAttribute('style', 'min-height:' + window_height);
-})();
+// Instagram Backbone Module
+var Gram = Backbone.Model.extend({
+  initialize: function() {
+    console.log('Gram Model initialized.');
+  }
+});
+
+var Grams = Backbone.Collection.extend({
+  model: Gram,
+  url: 'https://api.instagram.com/v1/users/11919494/media/recent',
+});
+
+// Do all the stuff on document ready
+$(document).ready(function() {
+  setAsideHeight();
+});
